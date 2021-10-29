01dbaf8b8acf252e12e81b3bd88ba58812a34dc3
zachmyu <zach.m.yu@gmail.com>
2021-10-28 23:52:00 -0700

    Updated form dependancy

--- app/forms/login_form.py
+++ app/forms/login_form.py
@@ -8,23 +8,22 @@ def user_exists(form, field):
     # Checking if user exists
     email = field.data
     user = User.query.filter(User.email == email).first()
-    if user:
-        raise ValidationError('Email address is already in use.')
+    if not user:
+        raise ValidationError('Email provided not found.')
 
 
-def username_exists(form, field):
-    # Checking if username is already in use
-    username = field.data
-    user = User.query.filter(User.username == username).first()
-    if user:
-        raise ValidationError('Username is already in use.')
+def password_matches(form, field):
+    # Checking if password matches
+    password = field.data
+    email = form.data['email']
+    user = User.query.filter(User.email == email).first()
+    if not user:
+        raise ValidationError('No such user exists.')
+    if not user.check_password(password):
+        raise ValidationError('Password was incorrect.')
 
 
-class SignUpForm(FlaskForm):
-    first_name = StringField('First Name', validators=[DataRequired()])
-    last_name = StringField('Last Name', validators=[DataRequired()])
-    username = StringField(
-        'username', validators=[DataRequired(), username_exists])
+class LoginForm(FlaskForm):
     email = StringField('email', validators=[DataRequired(), user_exists])
-    password = StringField('password', validators=[DataRequired()])
-    # profile_image_url = StringField('profile_image_url', validators=[DataRequired()])
+    password = StringField('password', validators=[
+                           DataRequired(), password_matches])
