c5a0e08d4a970495409d80f3a8dcc9c20088da5a
zachmyu <zach.m.yu@gmail.com>
2021-10-28 23:44:36 -0700

    Updated all seeder files

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,14 @@ from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_login import LoginManager
 
-from .models import db, User
+from .models import db, User, Date_type, Venue, Reservation, Review, Favorite
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
+from .api.image_routes import image_routes
+from .api.venue_routes import venue_routes
+from .api.review_routes import review_routes
+from .api.reservation_routes import reservation_routes
+from .api.favorite_routes import favorite_routes
 
 from .seeds import seed_commands
 
@@ -31,6 +36,11 @@ app.cli.add_command(seed_commands)
 app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
+app.register_blueprint(image_routes, url_prefix='/api/images')
+app.register_blueprint(venue_routes, url_prefix='/api/venues')
+app.register_blueprint(review_routes, url_prefix='/api/reviews')
+app.register_blueprint(reservation_routes, url_prefix='/api/reservations')
+app.register_blueprint(favorite_routes, url_prefix='/api/favorites')
 db.init_app(app)
 Migrate(app, db)
 
--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,82 +1,35 @@
-import os
-from flask import Flask, render_template, request, session, redirect
-from flask_cors import CORS
-from flask_migrate import Migrate
-from flask_wtf.csrf import CSRFProtect, generate_csrf
-from flask_login import LoginManager
+from flask.cli import AppGroup
+from .users import seed_users, undo_users
+from .date_types import seed_date_types, undo_date_types
+from .venues import seed_venues, undo_venues
+from .reservations import seed_reservations, undo_reservations
+from .reviews import seed_reviews, undo_reviews
+from .favorites import seed_favorites, undo_favorites
 
-from .models import db, User, Date_type, Venue, Reservation, Review, Favorite
-from .api.user_routes import user_routes
-from .api.auth_routes import auth_routes
-from .api.image_routes import image_routes
-from .api.venue_routes import venue_routes
-from .api.review_routes import review_routes
-from .api.reservation_routes import reservation_routes
-from .api.favorite_routes import favorite_routes
-
-from .seeds import seed_commands
-
-from .config import Config
-
-app = Flask(__name__)
-
-# Setup login manager
-login = LoginManager(app)
-login.login_view = 'auth.unauthorized'
+# Creates a seed group to hold our commands
+# So we can type `flask seed --help`
+seed_commands = AppGroup('seed')
 
 
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
+# Creates the `flask seed all` command
+@seed_commands.command('all')
+def seed():
+    seed_users()
+    seed_date_types()
+    seed_venues()
+    seed_reservations()
+    seed_reviews()
+    seed_favorites()
+    # Add other seed functions here
 
 
-# Tell flask about our seed commands
-app.cli.add_command(seed_commands)
-
-app.config.from_object(Config)
-app.register_blueprint(user_routes, url_prefix='/api/users')
-app.register_blueprint(auth_routes, url_prefix='/api/auth')
-app.register_blueprint(image_routes, url_prefix='/api/images')
-app.register_blueprint(venue_routes, url_prefix='/api/venues')
-app.register_blueprint(review_routes, url_prefix='/api/reviews')
-app.register_blueprint(reservation_routes, url_prefix='/api/reservations')
-app.register_blueprint(favorite_routes, url_prefix='/api/favorites')
-db.init_app(app)
-Migrate(app, db)
-
-# Application Security
-CORS(app)
-
-
-# Since we are deploying with Docker and Flask,
-# we won't be using a buildpack when we deploy to Heroku.
-# Therefore, we need to make sure that in production any
-# request made over http is redirected to https.
-# Well.........
-@app.before_request
-def https_redirect():
-    if os.environ.get('FLASK_ENV') == 'production':
-        if request.headers.get('X-Forwarded-Proto') == 'http':
-            url = request.url.replace('http://', 'https://', 1)
-            code = 301
-            return redirect(url, code=code)
-
-
-@app.after_request
-def inject_csrf_token(response):
-    response.set_cookie(
-        'csrf_token',
-        generate_csrf(),
-        secure=True if os.environ.get('FLASK_ENV') == 'production' else False,
-        samesite='Strict' if os.environ.get(
-            'FLASK_ENV') == 'production' else None,
-        httponly=True)
-    return response
-
-
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def react_root(path):
-    if path == 'favicon.ico':
-        return app.send_static_file('favicon.ico')
-    return app.send_static_file('index.html')
+# Creates the `flask seed undo` command
+@seed_commands.command('undo')
+def undo():
+    undo_users()
+    undo_date_types()
+    undo_venues()
+    undo_reservations()
+    undo_reviews()
+    undo_favorites()
+    # Add other undo functions here
