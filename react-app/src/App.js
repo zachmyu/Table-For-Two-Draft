dfb6c4961acdbd9a893c09a7ec4a35224fe225f3
zachmyu <zach.m.yu@gmail.com>
2021-11-07 22:53:12 -0800

    Updated App.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,17 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import LoginForm from './components/auth/LoginForm';
-import SignUpForm from './components/auth/SignUpForm';
 import NavBar from './components/NavBar/NavBar';
+// import Footer from './components/NavBar/Footer';
 import ProtectedRoute from './components/auth/ProtectedRoute';
-import UsersList from './components/UsersList';
-import User from './components/User';
+// import UsersList from './components/UsersList';
+// import User from './components/User';
+// import Calendar from './components/Calendar'
+// import DropDown from './components/DropDown'
+// import GoogleMap from './components/Venue/GoogleMap'
+// import Venue from './components/Venue'
 import { authenticate } from './store/session';
+// import HomePage from './components/HomePage'
+// import Search from './components/Search'
+
+
 
 function App() {
     const [loaded, setLoaded] = useState(false);
     const dispatch = useDispatch();
+    const [results, setResults] = useState([])
 
     useEffect(() => {
         (async () => {
@@ -25,26 +33,38 @@ function App() {
     }
 
     return (
-        <BrowserRouter>
-            <NavBar />
-            <Switch>
-                <Route path='/login' exact={true}>
-                    <LoginForm />
-                </Route>
-                <Route path='/sign-up' exact={true}>
-                    <SignUpForm />
-                </Route>
-                <ProtectedRoute path='/users' exact={true} >
-                    <UsersList />
-                </ProtectedRoute>
-                <ProtectedRoute path='/users/:userId' exact={true} >
-                    <User />
-                </ProtectedRoute>
-                <ProtectedRoute path='/' exact={true} >
-                    <h1>My Home Page</h1>
-                </ProtectedRoute>
-            </Switch>
-        </BrowserRouter>
+        <>
+            <NavBar loaded={loaded} />
+            {loaded && (
+                <Switch>
+                    {/* <Route exact path='/calendar'>
+                        <Calendar></Calendar>
+                    </Route> */}
+                    {/* <Route exact path='/dropdown'>
+                        <DropDown></DropDown>
+                    </Route> */}
+                    {/* <Route exact path='/google-map'>
+                        <GoogleMap></GoogleMap>
+                    </Route> */}
+                    {/* <Route exact path='/venues/:id'>
+                        <Venue></Venue>
+                    </Route> */}
+                    {/* <Route exact path='/search'>
+                        <Search results={results} />
+                    </Route> */}
+                    {/* <Route exact path='/' >
+                        <HomePage setResults={setResults} />
+                    </Route> */}
+                    {/* <ProtectedRoute exact path='/users'>
+                        <UsersList />
+                    </ProtectedRoute> */}
+                    {/* <ProtectedRoute exact path='/users/:userId'>
+                        <User />
+                    </ProtectedRoute> */}
+                </Switch>
+            )}
+            {/* <Footer /> */}
+        </>
     );
 }
 
