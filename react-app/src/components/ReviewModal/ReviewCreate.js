488faedbab59ef86ddf1bb879039c3de6ec2dfad
zachmyu <zach.m.yu@gmail.com>
2021-11-14 21:25:56 -0800

    Updated Review Create component, resolved bugs, changes in state detected

--- react-app/src/components/ReviewModal/ReviewCreate.js
+++ react-app/src/components/ReviewModal/ReviewCreate.js
@@ -1,22 +1,35 @@
 import React, { useState } from 'react';
-import { useHistory } from 'react-router';
 import { useDispatch, useSelector } from 'react-redux';
-// import { Modal } from '../../context/Modal';
-import { createReview } from '../../store/reviews'
+import { useHistory } from 'react-router';
+import { Modal } from '../../context/Modal';
+import { createReview } from '../../store/review'
 
-function ReviewFormModal({ venue_id, reviewId }) {
+import "./ReviewForm.css"
+
+function ReviewCreate({ venue_id }) {
     const dispatch = useDispatch();
     const history = useHistory()
-    const sessionUser = useSelector((state) => state.session.user)
+
+    const [showModal, setShowModal] = useState(false);
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [rating, setRating] = useState(0);
-    venue_id = Number(venue_id)
+
+    const sessionUser = useSelector((state) => state.session.user)
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        dispatch(createReview({ user_id: sessionUser.id, venue_id, title, body, rating }))
-        history.push(`/users/${sessionUser.id}`)
+        const reviewDetails = {
+            user_id: sessionUser.id,
+            venue_id,
+            title,
+            body,
+            rating
+        }
+        console.log(reviewDetails)
+        dispatch(createReview(reviewDetails))
+        setShowModal(false)
+        // history.push(`/users/${sessionUser.id}`)
 
     }
 
@@ -33,8 +46,9 @@ function ReviewFormModal({ venue_id, reviewId }) {
                     key={i}
                     value={i}
                     checked={i === rating}
-                    onChange={(e) => setRating(i)}
-                    onClick={() => ratingHelper(i)}>
+                    // onChange={(e) => setRating(i)}
+                    onClick={() => ratingHelper(i)}
+                >
                 </input>
             </div>
         ))
@@ -42,36 +56,43 @@ function ReviewFormModal({ venue_id, reviewId }) {
 
     return (
         <>
-            <form className='review-container' onSubmit={handleSubmit}>
-                <h3>Add a review for this venue</h3>
-                <div className="review-element-container">
-                    <input
-                        className="review-element"
-                        placeholder='Title'
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                        required
-                    />
-                </div>
-                <div className="review-element-container">
-                    <textarea
-                        className="review-text-element"
-                        value={body}
-                        placeholder='Add your review!'
-                        onChange={(e) => setBody(e.target.value)}
-                        required
-                    />
-                </div>
-                <div className="review-radio-container">
-                    <h3>Rating</h3>
-                    {radioHelper()}
-                </div>
-                <div className='review-button-container'>
-                    <button className="button2" type="submit">Submit Review</button>
-                </div>
-            </form >
+            <button className="button2" onClick={() => setShowModal(true)}>Create a review</button>
+            {showModal && (
+                <Modal onClose={() => setShowModal(false)}>
+                    <form className='review-container' onSubmit={handleSubmit}>
+                        <h3>Add a review for this venue</h3>
+                        <div className="review-element-container">
+                            <input
+                                className="review-element"
+                                placeholder='Title'
+                                value={title}
+                                onChange={(e) => setTitle(e.target.value)}
+                                required
+                            />
+                        </div>
+                        <div className="review-element-container">
+                            <textarea
+                                className="review-text-element"
+                                value={body}
+                                placeholder='Add your review!'
+                                onChange={(e) => setBody(e.target.value)}
+                                required
+                            />
+                        </div>
+                        <div className="review-radio-container">
+                            <h3>Rating</h3>
+                            {radioHelper()}
+                        </div>
+                        <div className='review-button-container'>
+                            <button className="button2" type="submit">Submit Review</button>
+                        </div>
+                    </form >
+                </Modal>
+
+            )}
+
         </>
     );
 }
 
-export default ReviewFormModal;
+export default ReviewCreate;
