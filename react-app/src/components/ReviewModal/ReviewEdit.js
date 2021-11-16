b4468aa07a5329e2bd0266884489de1e9ab49744
zachmyu <zach.m.yu@gmail.com>
2021-11-16 00:45:25 -0800

    Updated review edit to show as modal

--- react-app/src/components/ReviewModal/ReviewEdit.js
+++ react-app/src/components/ReviewModal/ReviewEdit.js
@@ -1,25 +1,53 @@
 import React, { useState } from 'react';
-import { useHistory } from 'react-router';
+// import { useHistory } from 'react-router';
 import { useDispatch, useSelector } from 'react-redux';
-// import { Modal } from '../../context/Modal';
-import { createReview } from '../../store/reviews'
+import { Modal } from '../../context/Modal';
+import { updateReview } from '../../store/review'
 
-function ReviewCreate({ venue_id, reviewId }) {
-    const dispatch =
-        useDispatch();
-    const history = useHistory()
+function ReviewEdit({ venue_id, review }) {
+    const dispatch = useDispatch();
+    // const history = useHistory()
+    const [showModal, setShowModal] = useState(false);
+    const [title, setTitle] = useState(review.title);
+    const [body, setBody] = useState(review.body);
+    const [rating, setRating] = useState(review.rating);
+
     const sessionUser = useSelector((state) => state.session.user)
-    const [title, setTitle] = useState('');
-    const [body, setBody] = useState('');
-    const [rating, setRating] = useState(0);
-    venue_id = Number(venue_id)
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        dispatch(createReview({ user_id: sessionUser.id, venue_id, title, body, rating }))
-        history.push(`/users/${sessionUser.id}`)
+        const reviewDetails = {
+            user_id: sessionUser.id,
+            venue_id,
+            title,
+            body,
+            rating,
+            review_id: review.id
+        }
+        console.log(reviewDetails)
+        dispatch(updateReview(reviewDetails))
+        setTitle("")
+        setBody("")
+        setRating(0)
+        setShowModal(false)
+        // history.push(`/users/${sessionUser.id}`)
 
     }
+
+    const openForm = (review) => {
+        // setShowForm(!showForm)
+        // setTitle(review.title)
+        // setBody(review.body)
+        // setRating(review.rating)
+        // setFormId(review.id)
+    }
+
+    // const deleteSingleReview = async (reviewId) => {
+    //     let alert = window.confirm('Are you sure you want to delete this review?')
+    //     if (alert) {
+    //         await dispatch(deleteReview(reviewId))
+    //     }
+    // }
 
     const ratingHelper = (num) => {
         setRating(num)
@@ -43,36 +71,41 @@ function ReviewCreate({ venue_id, reviewId }) {
 
     return (
         <>
-            <form className='review-container' onSubmit={handleSubmit}>
-                <h3>Add a review for this venue</h3>
-                <div className="review-element-container">
-                    <input
-                        className="review-element"
-                        placeholder='Title'
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                        required
-                    />
-                </div>
-                <div className="review-element-container">
-                    <textarea
-                        className="review-text-element"
-                        value={body}
-                        placeholder='Add your review!'
-                        onChange={(e) => setBody(e.target.value)}
-                        required
-                    />
-                </div>
-                <div className="review-radio-container">
-                    <h3>Rating</h3>
-                    {radioHelper()}
-                </div>
-                <div className='review-button-container'>
-                    <button className="button2" type="submit">Submit Review</button>
-                </div>
-            </form >
+            <button className='button3' onClick={() => setShowModal(true)}>Edit Review</button>
+            {showModal && (
+                <Modal onClose={() => setShowModal(false)}>
+                    <form className='review-container' onSubmit={handleSubmit}>
+                        <h3>Edit your review for this venue</h3>
+                        <div className="review-element-container">
+                            <input
+                                className="review-element"
+                                placeholder='Title'
+                                value={title}
+                                onChange={(e) => setTitle(e.target.value)}
+                                required
+                            />
+                        </div>
+                        <div className="review-element-container">
+                            <textarea
+                                className="review-text-element"
+                                value={body}
+                                placeholder='Add your review!'
+                                onChange={(e) => setBody(e.target.value)}
+                                required
+                            />
+                        </div>
+                        <div className="review-radio-container">
+                            <h3>Rating</h3>
+                            {radioHelper()}
+                        </div>
+                        <div className='review-button-container'>
+                            <button className="button2" type="submit">Submit Review</button>
+                        </div>
+                    </form >
+                </Modal>
+            )}
         </>
     );
 }
 
-export default ReviewCreate;
+export default ReviewEdit;
