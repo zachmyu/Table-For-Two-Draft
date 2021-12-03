bf48e76eecd2eb535dddf69e995e61d5348183fb
zachmyu <zach.m.yu@gmail.com>
2021-12-02 23:55:17 -0800

    Updated reservation backend

--- app/models/reservation.py
+++ app/models/reservation.py
@@ -13,16 +13,16 @@ class Reservation(db.Model, UserMixin):
     reservation_datetime = db.Column(db.DateTime, nullable=False)
     party_size = db.Column(db.Integer, nullable=False)
     duration = db.Column(db.Float, nullable=False)
-    
+
     user = db.relationship('User', back_populates='reservations')
     venue = db.relationship('Venue', back_populates='reservations')
-    
 
     def to_dict(self):
         return {
             'id': self.id,
             'user_id': self.user_id,
-            'venue_id': self.venue_id,
+            # 'venue_id': self.venue_id,
+            'venue': self.venue.to_dict(),
             'reservation_datetime': self.reservation_datetime,
             'party_size': self.party_size,
             'duration': self.duration,
