6b6b04ce52a549ce6634be33d685d499f5fb72dd
zachmyu <zach.m.yu@gmail.com>
2022-01-09 20:03:47 -0800

    Completed Calendar component, fixed issue with wrong date

--- react-app/src/components/Calendar/Calendar.js
+++ react-app/src/components/Calendar/Calendar.js
@@ -1,47 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import TextField from '@mui/material/TextField';
-import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
-import TimePicker from '@mui/lab/TimePicker';
-import Stack from '@mui/material/Stack';
+import DateTimePicker from '@mui/lab/DateTimePicker';
 
-//! Need to change database so date and time are separate!
-
-function Calendar() {
-    const [date, setDate] = useState(new Date(new Date().setMinutes(0)))
-    const [time, setTime] = useState(new Date(new Date().setMinutes(0)))
-
+function Calendar({ dateTime, setDateTime }) {
     return (
-        <Stack component="form" noValidate spacing={3}>
-            {/* <DateTimePicker
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <DateTimePicker
+                renderInput={(props) => <TextField {...props} />}
                 label="Reservation Date & Time"
                 value={dateTime}
                 onChange={(newValue) => {
                     setDateTime(newValue);
                 }}
-                // renderInput={(params) => <TextField {...params} />}
-                shouldDisableTime={(timeValue, clockType) => {
-                    if (clockType === 'minutes' && timeValue % 30) {
-                        return true;
-                    }
-                    return false;
-                }}
-            /> */}
-            <DesktopDatePicker
-                label="Reservation Date"
-                value={date}
-                minDate={new Date('2017-01-01')}
-                onChange={(newValue) => {
-                    setDate(newValue);
-                }}
-                renderInput={(params) => <TextField {...params} />}
-            />
-            <TimePicker
-                renderInput={(params) => <TextField {...params} />}
-                label="Reservation Time"
-                value={time}
-                onChange={(newValue) => {
-                    setTime(newValue);
-                }}
                 shouldDisableTime={(timeValue, clockType) => {
                     if (clockType === 'minutes' && timeValue % 30) {
                         return true;
@@ -49,7 +21,7 @@ function Calendar() {
                     return false;
                 }}
             />
-        </Stack>
+        </LocalizationProvider>
     );
 }
 
