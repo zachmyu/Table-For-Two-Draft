61bda92d620227ac8e3ae29d55afb1eaa63392f0
zachmyu <zach.m.yu@gmail.com>
2022-01-11 22:36:15 -0800

    updated to remove unused variables

--- react-app/src/components/Venue/Venue.js
+++ react-app/src/components/Venue/Venue.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from 'react-redux'
 import { getSingleVenue } from '../../store/venue'
 import { useParams } from "react-router-dom";
