cab18778b212b99794e993636196c56cbb77bbbc
zachmyu <zach.m.yu@gmail.com>
2022-01-19 23:09:39 -0800

    Updated review routes for organization

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -1,4 +1,5 @@
 from app.forms import ReviewForm
+from flask_login import login_required
 from flask import Blueprint, request, jsonify
 
 from app.models import Review, db
@@ -21,41 +22,28 @@ def review(id):
     return review.to_dict()
 
 
-# READ ALL - Query by venues = Reviews
+# READ ALL: Query by venues - Reviews
 @review_routes.route('/venues/<int:venue_id>/')
 def reviews_by_venues(venue_id):
     reviews = Review.query.filter_by(venue_id=venue_id).all()
     return {review.id: review.to_dict() for review in reviews}
 
 
-# READ ALL - Query by users = Reviews
+# READ ALL: Query by users = Reviews
 @review_routes.route('/users/<int:user_id>/')
 def ratings_by_users(user_id):
     reviews = Review.query.filter_by(user_id=user_id).all()
     return {review.id: review.to_dict() for review in reviews}
 
 
-# UPDATE - Review
-@review_routes.route('/<int:id>/', methods=['PUT'])
-def review_edit(id):
-    form = ReviewForm()
-    form['csrf_token'].data = request.cookies['csrf_token']
-    if form.validate_on_submit():
-        edit_review = Review.query.get(id)
-        form.populate_obj(edit_review)
-        db.session.commit()
-        return edit_review.to_dict()
-    print("Unable to validate: ", form.errors)
-    return {'errors': form.errors}
-
-
 # CREATE - Review
-@review_routes.route('/venues/<int:id>/', methods=['POST'])
-def new_review(id):
+@review_routes.route('/venues/', methods=['POST'])
+@login_required
+def new_review():
     request_json = request.get_json()
     review = Review(
         user_id=request_json["user_id"],
-        venue_id=id,
+        venue_id=request_json["venue_id"],
         title=request_json["title"],
         body=request_json['body'],
         rating=request_json['rating']
@@ -65,11 +53,26 @@ def new_review(id):
     return {'review': review.to_dict()}
 
 
+# UPDATE - Review
+@review_routes.route('/<int:id>/', methods=['PUT'])
+@login_required
+def review_edit(id):
+    form = ReviewForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        review_to_edit = Review.query.get(id)
+        form.populate_obj(review_to_edit)
+        db.session.commit()
+        return review_to_edit.to_dict()
+    print("Unable to validate: ", form.errors)
+    return {'errors': form.errors}
+
+
 # DELETE - Review
 @review_routes.route('/<int:id>/', methods=['DELETE'])
-def delete_review_by_id(id):
+@login_required
+def review_delete(id):
     delete_review = Review.query.get(id)
     db.session.delete(delete_review)
     db.session.commit()
-    # return {'delete_review': delete_review.to_dict()}
     return "Review deleted"
